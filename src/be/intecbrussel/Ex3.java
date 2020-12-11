39aa840d319b7a3492fbd8c8bb756043a37eca2f
codergo7 <codergo7@gmail.com>
2020-12-11 12:25:24 +0100

    added third exercise

---
+++ src/be/intecbrussel/Ex3.java
@@ -0,0 +1,22 @@
+package be.intecbrussel;
+
+import java.util.stream.Stream;
+
+/**
+ * Maak een stream die de eerste twee strings in hoofdletter gaat plaatsen en
+ * dan de 2 strings gaat combineren naar 1 strings.
+ */
+public class Ex3 {
+
+    public static void main(String[] args) {
+
+        String[] greetings = {"Hello","World", "Merhaba", "Hallo",  "Salut"};
+
+        method3(greetings);
+    }
+
+    private static void method3(String[] greetings) {
+        String str = Stream.of(greetings).map(s -> s.toUpperCase()).limit(2).reduce("",(s, s2) -> s+s2);
+        System.out.println(str);
+    }
+}
